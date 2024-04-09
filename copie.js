6bfa6eaedadb8db6b1ffdbaa741943c61b0abcc5
Olivier-Miguet1 <olivier.miguet1@gmail.com>
2024-04-09 15:48:51 +0200

    Sauvegarde de l'exo copie

---
+++ copie.js
@@ -0,0 +1,15 @@
+let variablesimple1 = 12
+let variablesimple2 = variablesimple1
+variablesimple1 = 35
+
+console.log("variablesimple1", variablesimple1)
+console.log("variablesimple2", variablesimple2)
+
+let variableComplexe1 = ["informatique", "sport", "television"]
+let variableComplexe2 = variableComplexe1
+let variableComplexe3 = [...variableComplexe1]
+
+
+console.log("variableComplexe1", variableComplexe1)
+console.log("variableComplexe2", variableComplexe2)
+console.log("variableComplexe3", variableComplexe3)
