a77c484509ee09489583e90017972a0a12c5e0ee
Olivier-Miguet1 <olivier.miguet1@gmail.com>
2024-04-09 10:23:10 +0200

    Sauvegarde d'exo moyenne de livre vendu/jour

---
+++ moyenne_par_j_livre_vendu.js
@@ -0,0 +1,6 @@
+let totalLivreSemaine = 600;
+let ajoutLivreVendu = 50;
+let totalLivreVendu = totalLivreSemaine + ajoutLivreVendu;
+let moyenneJourLivreVendu = totalLivreVendu / 7;
+console.log("Le nombre de livres vendus par jour est de " + Math.round(moyenneJourLivreVendu));
+
