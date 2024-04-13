7f32a9cf1d53c57c31af3c6ff7fe970fe26edb33
Olivier-Miguet1 <olivier.miguet1@gmail.com>
2024-04-13 17:43:05 +0200

    Premier essai playlist.js

--- playlist.js
+++ playlist.js
@@ -1,7 +1,18 @@
+let playList = {
+    morceau1 : "The road to hell",
+    morceau2 : "Capitaine abandonné",
+    morceau3 : "Daytona",
+    morceau4 : "Highway to Hell",
+    morceau5 : "Josephine",               // 5 morceaux de ma playlist
+}
 
-let playList = ["The road to hell", "Daytona", "Josephine"];
+let inventaireCD = 17                     // Nombre de cd
 
-let playlist = 3,
-let totalMorceaux = let playList
+let playList3 = ["The road to hell", "Daytona", "Josephine"];   // Titres des morceaux de ma playlist
 
-console.log(),
+let totalMorceaux = 3                     // Nombre de morceaux
+let totalMorceauxFriends = totalMorceaux;
+totalMorceaux = 39                        // Nombre de morceaux de la team
+
+let ajoutTotalMorceauxFriends = 24
+let totalMorceauxWithFriends = totalMorceaux + ajoutTotalMorceauxFriends;
