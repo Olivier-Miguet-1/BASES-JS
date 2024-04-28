94653613cc7bd0f26e8fe26d83d7a618785e30b1
Olivier-Miguet-1 <olivier.miguet1>
2024-04-28 20:01:08 +0200

    Commentaires dans function in type

--- function_in_type.js
+++ function_in_type.js
@@ -1,15 +1,17 @@
 const _ = require('lodash')
 
-
+// On commence par créer une liste de types de cartes ("A" pour As, "2" pour 2, etc.) 
+//et une liste de types de cartes ("CA" pour cœur, "C" pour carreau, etc.).
 var tab_element = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "V", "D", "R"]
 
 var type = ["CA", "C", "P", "T"]
 
-
+//Ensuite, on crée une liste vide appelée pile_card
 var pile_card = []
 
 
-
+// On fait une double boucle pour combiner chaque type de carte avec chaque 
+//type de carte ("A-CA", "A-C", etc.) et on ajoute ces combinaisons à la liste pile_card.
 
 for (var i = 0; i < tab_element.length; i++) {
     for (var j = 0; j < type.length; j++) {
@@ -17,8 +19,8 @@ for (var i = 0; i < tab_element.length; i++) {
     }
 }
 
-
-
+// On utilise des fonctions pour mélanger les cartes. La première utilise un algorithme simple
+// pour mélanger les cartes. Les deux autres utilisent une fonction aléatoire.
 function shuffle(tab) {
     return tab.sort(function () { return Math.random() - 0.5 })
 }
@@ -34,6 +36,8 @@ function shuffleFisher(arr) {
 }
 
 console.log(pile_card)
+
+// On utilise une fonction pour mélanger les cartes dans pile_card.
 pile_card = shuffle(pile_card)
 
 
@@ -46,7 +50,10 @@ function numberRamdom(max, min) {
 
 //console.log(turn1, pile_card[turn1])
 
-
+// On définit une fonction pour attribuer des valeurs et des
+// couleurs aux cartes. Par exemple, les "A" ont une valeur de
+// 11, les autres cartes numériques gardent leur valeur numérique,
+// et les autres cartes (V, D, R) ont une valeur de 10.
 
 
 pile_card = pile_card.map(function (cart) {
@@ -57,6 +64,9 @@ pile_card = pile_card.map(function (cart) {
         value = 10
     else if (isNaN(value) && value == "A")
         value = 11
+    // Les couleurs sont déterminées en fonction du type de carte. Les
+    // cœurs et les carreaux sont rouges, les piques et les trèfles sont
+    // noirs. On applique cette fonction à chaque carte dans la liste pile_card.
     else
         value = Number(value)
     if (value_color == "CA" || value_color == "C")
@@ -67,13 +77,13 @@ pile_card = pile_card.map(function (cart) {
     return obj
 })
 
+// On affiche les cartes avec leurs valeurs et couleurs attribuées.
 console.log(pile_card)
 
+//on utilise une fonction spéciale de la bibliothèque lodash pour mélanger une
+// dernière fois les cartes de manière aléatoire, puis on affiche la liste de cartes mélangées.
 pile_card = _.shuffle(pile_card)
 console.log(pile_card)
-
-var indexFind = _.findIndex(pile_card, ["name", "4-C"])
-console.log(indexFind, pile_card(indexFind))
 
 
 
